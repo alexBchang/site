7e8f12edb77645cb01751d72d445c55a372a477c
alexbchang <alexbchang@gmail.com>
2019-08-11 22:51:57 -0700

    use images

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,12 @@ import * as React from 'react'
 
 const Index = () => (
   <div>
-    <p>Hello World!</p>
+    <h1>alex chang</h1>
+    <h2>growing every day</h2>
+    <img
+      src="/static/images/index/background_m.jpg"
+      alt="plant growing in front of a drainage pipe"
+    />
   </div>
 )
 
